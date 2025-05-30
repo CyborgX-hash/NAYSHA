174eeb12f9515713d92b5c3406c0df57496c9650
CyborgX-hash <sakshamsontakke713@gmail.com>
2025-05-30 19:43:52 +0530

    contact section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,49 +24,55 @@ function Home() {
         <h1>TOP RECOMMENDED DESTINATION</h1>
       </div>
       <div className="container">
-        <div className="item"><img src='/images/goa.jpeg'/>
+        <div className="item"><img src='/images/goa.jpeg' alt="Goa"/>
         <div className="item-overlay">
           <h2>Goa, India</h2>
           <p>Beaches, parties & seafood</p>
         </div>
         </div>
-        <div className="item"><img src='images/darjeeling.jpeg'/>
+        <div className="item"><img src='images/darjeeling.jpeg' alt='dar'/>
         <div className="item-overlay">
           <h2>Darjeeling, India</h2>
           <p>Misty hills, scenic views, and tea gardens.</p>
         </div>
         </div>
-        <div className="item"><img src='/images/manali.jpg'/>
+        <div className="item"><img src='/images/manali.jpg' alt='manali'/>
         <div className="item-overlay">
             <h2>Manali, India</h2>
             <p>Snowy hills, perfect for hiking and skiing.</p>
           </div>
         </div>
-        <div className="item"><img src='/images/leh.jpeg'/>
+        <div className="item"><img src='/images/leh.jpeg' alt='leh'/>
         <div className="item-overlay">
           <h2>Leh-Ladakh, India</h2>
           <p>High altitude deserts, monasteries, and biking trails.</p>
         </div>
         </div>
-        <div className="item"><img src='/images/paris.jpg'/>
+        <div className="item"><img src='/images/paris.jpg' alt='paris'/>
         <div className="item-overlay">
           <h2>Paris, France</h2>
           <p>The city of love, art, and iconic monuments.</p>
         </div>
         </div>
-        <div className="item"><img src='/images/bali.jpeg'/>
+        <div className="item"><img src='/images/bali.jpeg' alt='bali'/>
         <div className="item-overlay">
           <h2>Bali, Indonesia</h2>
           <p>Tropical beaches, temples, and vibrant culture.</p>
         </div>
         </div>
         <div className="about">
-          <h2>About NAYSHA</h2>
+          <h2>ABOUT NAYSHA</h2>
           <p>
             NAYSHA is your smart travel companion that helps you discover amazing places based on your budget. 
             Just enter how much you’re willing to spend, and we’ll recommend destinations that match your needs. 
             Simple, fast, and budget-friendly — travel planning made easy!
           </p>
+        </div>
+        <div className='contact'>
+          <h2>CONTACT US</h2>
+          <p>Email :- Naysha713@gmail.com</p>
+          <p>Ph No.:-  8828 1570 7021</p>
+          <p>Location :- Newton School Of Technology Delhi NCR.</p>
         </div>
 
         
